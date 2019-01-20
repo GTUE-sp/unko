76342aa3393fe6103480a0ea232e20dea4371991
youta ogino <dev.youta1119@gmail.com>
2019-01-20 17:31:42 +0900

    ポチポチできるね

--- src/views/Table.vue
+++ src/views/Table.vue
@@ -1,99 +1,68 @@
 <template>
   <el-table
-    :data="tableData3"
-    style="width: 100%">
+    :data="studentsData"
+    stripe
+    style="width: 100%"
+    @cell-click="clickCell">
     <el-table-column
-      prop="date"
-      label="Name"
-      width="150">
+      prop="name"
+      align="center">
     </el-table-column>
-    <el-table-column label="Delivery Info">
+    <!-- 時間割　-->
+    <el-table-column
+        v-for="(subject, index) of timetableData.timetable"
+        :key="subject+'-'+index"
+        :label="String(index)"
+        header-align="center">
       <el-table-column
-        prop="name"
-        label="Name"
-        width="120">
-      </el-table-column>
-      <el-table-column label="Address Info">
-        <el-table-column
-          prop="state"
-          label="State"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="city"
-          label="City"
-          width="120">
-        </el-table-column>
-        <el-table-column
-          prop="address"
-          label="Address"
-          width="300">
-        </el-table-column>
-        <el-table-column
-          prop="zip"
-          label="Zip"
-          width="120">
-        </el-table-column>
+        :label="subject"
+        header-align="center"
+        align="center">
+          <template slot-scope="scope" v-if="scope.row.attendance_status[index] === 1">
+            <span> / </span>
+        </template>-->
       </el-table-column>
     </el-table-column>
   </el-table>
 </template>
 
 <script lang="ts">
-  import { Component, Vue } from 'vue-property-decorator';
+  import Vue from 'vue';
+  import { Component } from 'vue-property-decorator';
   import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import studentsJsonData from '@/mocks/get_students_response.json';
+  import timetableJsonData from '@/mocks/get_timetable_response.json';
+
+  interface Student {
+    id: string;
+    name: string;
+    attendance_number: string;
+    attendance_status: number[];
+  }
+
+  interface Timetable {
+    timetable: string[];
+  }
 
   @Component({})
-  export default class Home extends Vue {
-    public tableData3 = [{
-          date: '2016-05-03',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-02',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-04',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-01',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-08',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-06',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }, {
-          date: '2016-05-07',
-          name: 'Tom',
-          state: 'California',
-          city: 'Los Angeles',
-          address: 'No. 189, Grove St, Los Angeles',
-          zip: 'CA 90036'
-        }]
+  export default class Table extends Vue {
+    public studentsData: Student[] = [];
+    public timetableData: Timetable = timetableJsonData;
+    public beforeMount() {
+      const len = this.timetableData.timetable.length;
+      for (const student of studentsJsonData) {
+        this.studentsData.push(Object.assign(student, {attendance_status:  new Array<number>(len).fill(0)}))
+      }
+      this.studentsData[0].attendance_status[1] = 1
+    }
+
+    public destroyed() {
+      //this.attendance = {};
+    }
+    public clickCell(row: Student, column: any, cell: HTMLTableCellElement, event: any) {
+      const index = cell.cellIndex - 1;
+      const newValue = row.attendance_status[index] === 0 ? 1 : 0;
+      row.attendance_status.splice(index, 1, newValue);
+    }
   }
 </script>
